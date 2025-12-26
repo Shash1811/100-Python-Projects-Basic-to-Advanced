1cfc55340c2d2469e16a565fbaba4552ba5ef01b
Shash1811 <152872623+Shash1811@users.noreply.github.com>
2025-12-26 10:14:08 +0530

    Update main.py

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -3,12 +3,15 @@ from data import question_data
 from quiz_brain import QuizBrain
 question_bank = []
 for i in question_data:
-    question_text = i["text"]
-    question_ans = i["answer"]
+    question_text = i["question"]
+    question_ans = i["correct_answer"]
     question = Question(question_text,question_ans)
     question_bank.append(question)
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
 print("You've completed the quiz")
+
 print(f"You're  total score: {quiz.score}/{quiz.question_number}")
+
+#You can pick up as many questions as you like from Trivia api
